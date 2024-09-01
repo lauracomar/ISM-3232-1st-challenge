bf553fe1e05031ae0160833cca65ea027aea0904
Laura Comar <lautmcomar@gmail.com>
2024-09-01 18:12:46 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,12 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+// Task 2: Compound Data Types
+let products = ["ferrari, chili pepper, sunglasses"];
+const productdetails = {
+    productname = "ferrari"
+    price = 120.00
+    inStock = true
+}
+console.log (products, productdetails);
