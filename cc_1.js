6d76c714cf88b1613f4b4a87da3f23122727b234
Laura Comar <lautmcomar@gmail.com>
2024-09-01 17:26:33 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+// Task 1: Variables and Data Types
+let employeeName = "Carlos Sainz";
+const employeeID = 9011994;
+var isActive = true;
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
