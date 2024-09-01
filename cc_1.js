2ea8b2e05bd2166b22226aba733b807c6269fdb2
Laura Comar <lautmcomar@gmail.com>
2024-09-01 19:01:47 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -14,3 +14,17 @@ const productdetails = {
     inStock = true
 }
 console.log (products, productdetails);
+
+// Task 3: Assignment Operators
+let accountbalance = 200.00;
+console.log(accountbalance);
+accountbalance +=75.00;
+console.log (accountbalance); //275.00
+accountbalance -=20.00;
+console.log (accountbalance); // 255.00
+accountbalance *=5.00;
+console.log (accountbalance); // 1275.00
+accountbalance /=5.00;
+console.log (accountbalance); // 255.00
+accountbalance %=3.00;
+console.log (accountbalance); // 0.00
